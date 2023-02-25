b9575d9de381d6b41511898124eaefc45a82b28b
faisalsolangi <103946545+Faisalsolangi0@users.noreply.github.com>
2023-02-25 18:20:16 +0500

    Add files via upload

---
+++ Custom_Layout_19211@FaisalSolangi.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="./style.css">
+    <title></title>
+    <style type="text/css">
+
+    </style>
+
+</head>
+
+<body>
+
+
+    <div class="row">
+        <div id="box" class="col-12">
+            <h2 style="text-align: center; color: blue;">Image here</h2>
+        </div>
+    </div>
+
+    <div class="row">
+        <div id="side1" class="col-3">
+            <div class="row">
+                <div id="menu" class="col-4">
+                    <h3 style="text-align: center; margin-top: 60%;">menu here</h3>
+                </div>
+                <div id="content1" class="col-8">
+                    <h3 style="margin-top: 100%;">some content here</h3>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+        <div class="col-6">
+            <div class="row">
+                <div id="simple" class="col-12" style="border: 2px solid black;">
+                    <h3 style="color:gray">simple menu here</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div id="img" class="col-6">
+                    <h4>Image here</h4>
+                </div>
+                <div id="img" class="col-6">
+                    <h4>Image here</h4>
+                </div>
+                <div id="content2" class="col-6">
+                    <h3>some content in this area</h3>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div id="side2" class="col-3">
+        <div class="row">
+            <div id="login" class="col-4">
+                <h6 style="color: skyblue">Login-form</h6>
+                <h6 style="color: skyblue" id="form">username</h6>
+                <h6 style="color: skyblue" id="form">password</h6>
+                <h6 style="color: skyblue" id="form">submit</h6>
+            </div>
+
+            <div id="content3" class="col-8">
+                <h3 style="margin-top: 100%;">some content here</h3>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <div class="row">
+        <div id="footer" class="col-12">
+            <h3> &copy; copyright | Faisal-Solangi 20233 </h3>
+        </div>
+    </div>
+
+</body>
+
+</html>
